4ac3f801c4c7b130841a066b2c099cc2e35234c3
Yaroslav <Kaminski.y@mail.ru>
2019-06-25 16:33:31 +0300

    Функция Draw. позволяет не дублировать вывод информации

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,8 +10,25 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            int x1 = 7;
+            int y1 = 5;
+            char sum1 = '*';
+
+            Draw(x1, y1, sum1);
+
+            int x2 = 4;
+            int y2 = 5;
+            Char sum2 = '$';
+
+            Draw(x2, y2, sum2);
+
+
             Console.ReadLine();
+        }
+        static void Draw(int x, int y, Char sum)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sum);
         }
     }
 }
